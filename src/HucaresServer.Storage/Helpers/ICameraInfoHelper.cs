02f645499aaa70ffdfe64e5db7f4737646d51be9
ainoraz <ainoraz@gmail.com>
2018-09-23 15:42:26 +0300

    Added expected ICameraInfoHelper contract methods to be implamented

--- src/HucaresServer.Storage/Helpers/ICameraInfoHelper.cs
+++ src/HucaresServer.Storage/Helpers/ICameraInfoHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HucaresServer.Storage.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace HucaresServer.Storage.Helpers
 {
     public interface ICameraInfoHelper
     {
+        CameraInfo InsertCamera(string hostUrl, double latitude, double longtitude, bool trustedSource = false);
+        CameraInfo UpdateCameraSource(int id, string hostUrl, bool trustedSource);
+        CameraInfo UpdateCameraActivity(int id, bool inactive);
+        CameraInfo DeleteCameraById(int id);
+        IEnumerable<CameraInfo> GetAllCameras(bool? trustedSource = null);
+        IEnumerable<CameraInfo> GetActiveCameras(bool? trustedSource = null);
+        IEnumerable<CameraInfo> GetInactiveCameras();
     }
 }

650a3667140e67ed8a5c0705ae2437bdc35746ff
Andrius <andrejus.voitovas@gmail.com>
2018-09-23 12:51:54 +0100

    feat/missing-plate-wrapper. Added methods to Missing plate helper interface

--- src/HucaresServer.Storage/Helpers/IMissingPlateHelper.cs
+++ src/HucaresServer.Storage/Helpers/IMissingPlateHelper.cs
@@ -8,5 +8,49 @@ namespace HucaresServer.Storage.Helpers
 {
     interface IMissingPlateHelper
     {
+        /// <summary>
+        /// Insert new record
+        /// </summary>
+        /// <returns> bool </returns>
+        bool InsertRecord()
+        {
+
+        }
+
+        /// <summary>
+        /// Get all records from DB
+        /// </summary>
+        /// <returns> void </returns>
+        void GetAllRecords()
+        {
+
+        }
+
+        /// <summary>
+        /// Update record
+        /// </summary>
+        /// <returns> bool </returns>
+        bool UpdateRecord()
+        {
+
+        }
+
+        /// <summary>
+        /// If plate was found, mark as found plate
+        /// </summary>
+        /// <returns> bool </returns>
+        bool MarkFoundPlate()
+        {
+
+        }
+
+        /// <summary>
+        /// Delete plate from DB by ID
+        /// </summary>
+        /// <returns> void </returns>
+        void DeleteById()
+        {
+
+        }
     }
 }

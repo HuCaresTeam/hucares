33a446e18e8a88d8f13f49f1b9412e19d3947ba4
Andrius <andrejus.voitovas@gmail.com>
2018-09-23 15:25:55 +0100

    MLP wrapper changes

--- src/HucaresServer.Storage/Helpers/IMissingPlateHelper.cs
+++ src/HucaresServer.Storage/Helpers/IMissingPlateHelper.cs
@@ -12,54 +12,54 @@ namespace HucaresServer.Storage.Helpers
         /// <summary>
         /// Insert new record
         /// </summary>
-        /// <returns> void </returns>
         /// <param name="plateNumber">The string representation of the searched license plate number.</param>
         /// <param name="searchStartDatetime">Optional parameter, from what datetime to start querying detected license plates</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate InsertPlateRecord(string plateNumber, DateTime searchStartDatetime);
 
         /// <summary>
         /// Get all records from DB
         /// </summary>
-        /// <returns> All records from DB. </returns>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         IEnumerable<MissingLicensePlate> GetAllPlateRecords();
 
         /// <summary>
         /// Update record
         /// </summary>
-        /// <returns> MissingLicensePlate </returns>
         /// <param name="plateId">Plate id in the DB</param>
         /// <param name="plateNumber">The string representation of the searched license plate number.</param>
         /// <param name="searchStartDatetime">Optional parameter, from what datetime to start querying detected license plates</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate UpdatePlateRecord(int plateId, string plateNumber, DateTime searchStartDatetime);
 
         /// <summary>
         /// If plate was found, mark as found plate
         /// </summary>
-        /// <returns> MissingLicensePlate </returns>
         /// <param name="plateId">Plate id in the DB</param>
         /// <param name="requestDateTime">Optional parameter, from what datetime to start querying detected license plates</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate MarkFoundPlate(int plateId, DateTime requestDateTime);
 
         /// <summary>
         /// If plate was not found, mark as not found plate
         /// </summary>
-        /// <returns> MissingLicensePlate </returns>
         /// <param name="plateId">Plate id in the DB</param>
         /// <param name="requestDateTime">Optional parameter, from what datetime to start querying detected license plates</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate MarkNotFoundPlate(int plateId, DateTime requestDateTime);
 
         /// <summary>
         /// Delete plate from DB by ID
         /// </summary>
-        /// <returns> MissingLicensePlate </returns>
         /// <param name="plateId">Plate id in the DB</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate DeletePlateById(int plateId);
 
         /// <summary>
         /// Delete plate from DB by Number
         /// </summary>
-        /// <returns> MissingLicensePlate </returns>
         /// <param name="plateNumber">Plate number in the DB</param>
+        /// <returns> The stored MissingLicensePlate instance </returns>
         MissingLicensePlate DeletePlateByNumber(string plateNumber);
     }
 }

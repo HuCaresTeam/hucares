04c5bf55d872febc24cff3ed8a814ee54f35a150
ainoraz <ainoraz@gmail.com>
2018-09-23 16:07:16 +0300

    Added XML comments and updated methods

--- src/HucaresServer.Storage/Helpers/ICameraInfoHelper.cs
+++ src/HucaresServer.Storage/Helpers/ICameraInfoHelper.cs
@@ -9,12 +9,60 @@ namespace HucaresServer.Storage.Helpers
 {
     public interface ICameraInfoHelper
     {
-        CameraInfo InsertCamera(string hostUrl, double latitude, double longtitude, bool trustedSource = false);
-        CameraInfo UpdateCameraSource(int id, string hostUrl, bool trustedSource);
-        CameraInfo UpdateCameraActivity(int id, bool inactive);
+        /// <summary>
+        /// Inserts a camera record into the DB CameraInfo table.
+        /// </summary>
+        /// <param name="hostUrl"> Url for reaching the camera's footage </param>
+        /// <param name="latitude"> Geographical latitude of the camera</param>
+        /// <param name="longitude">Geographical longitude of the camera</param>
+        /// <param name="isTrustedSource">Determines if the source is trust worthy. Default is false. </param>
+        /// <returns> The stored CameraInfo instance </returns>
+        CameraInfo InsertCamera(string hostUrl, double latitude, double longitude, bool isTrustedSource = false);
+
+        /// <summary>
+        /// Updates the camera record in the DB CameraInfo table. 
+        /// </summary>
+        /// <param name="id"> Id of the record in the DB CameraInfo table </param>
+        /// <param name="newHostUrl">New host url for the camera</param>
+        /// <param name="newIsTrustedSource">New trusted source value for the camera</param>
+        /// <returns> The updated CameraInfo instance</returns>
+        CameraInfo UpdateCameraSource(int id, string newHostUrl, bool newIsTrustedSource);
+
+        /// <summary>
+        /// Sets whether the camera is still used for processing (IsActive) or not.
+        /// This is done because DLP records depend on camera locations.
+        /// </summary>
+        /// <param name="id"> Id of the record in the DB CameraInfo table </param>
+        /// <param name="isActive"> Sets whether the camera is active or not </param>
+        /// <returns> The updated CameraInfo instance </returns>
+        CameraInfo UpdateCameraActivity(int id, bool isActive);
+
+        /// <summary>
+        /// Deletes the camera record from the DB CameraInfo table. 
+        /// This should only succeed if no dependencies exist in DetectedLicensePlates DB table.
+        /// </summary>
+        /// <param name="id"> Id of the record in the DB CameraInfo table </param>
+        /// <returns> The deleted CameraInfo instance </returns>
         CameraInfo DeleteCameraById(int id);
-        IEnumerable<CameraInfo> GetAllCameras(bool? trustedSource = null);
-        IEnumerable<CameraInfo> GetActiveCameras(bool? trustedSource = null);
+
+        /// <summary>
+        /// Gets all camera records from the DB CameraInfoTable. Optionally, this request may be filtered by TrustedSource field.
+        /// </summary>
+        /// <param name="isTrustedSource">Optional paramater to filter by the TrustedSource field. Not filtered if null.</param>
+        /// <returns>IEnumerable of CameraInfo from the query result.</returns>
+        IEnumerable<CameraInfo> GetAllCameras(bool? isTrustedSource = null);
+
+        /// <summary>
+        /// Gets all active camera records from the DB CameraInfoTable. Optionally, this request may be filtered by TrustedSource field.
+        /// </summary>
+        /// <param name="isTrustedSource">Optional paramater to filter by the TrustedSource field. Not filtered if null.</param>
+        /// <returns>IEnumerable of CameraInfo from the query result.</returns>
+        IEnumerable<CameraInfo> GetActiveCameras(bool? isTrustedSource = null);
+
+        /// <summary>
+        /// Get all inactive camera records from the DB CameraInfoTable.
+        /// </summary>
+        /// <returns>IEnumerable of CameraInfo from the query result.</returns>
         IEnumerable<CameraInfo> GetInactiveCameras();
     }
 }

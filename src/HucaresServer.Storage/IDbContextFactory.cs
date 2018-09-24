6113c56b370cc7e2a3f5afea5664b59d353cabb5
ainoraz <ainoraz@gmail.com>
2018-09-24 16:23:53 +0300

    Added XML comment

--- src/HucaresServer.Storage/IDbContextFactory.cs
+++ src/HucaresServer.Storage/IDbContextFactory.cs
@@ -2,6 +2,10 @@
 {
     public interface IDbContextFactory
     {
+        /// <summary>
+        /// Builds and returns a new instance of HucaresContext.
+        /// </summary>
+        /// <returns>A new instance of HucaresContext.</returns>
         HucaresContext BuildHucaresContext();
     }
 }

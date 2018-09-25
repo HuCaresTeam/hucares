eac2a20edc7ee122f6cb9f6fe062767118d324ed
ainoraz <ainoraz@gmail.com>
2018-09-25 22:58:00 +0300

    IsActive default should be true

--- src/HucaresServer.Storage/Models/CameraInfo.cs
+++ src/HucaresServer.Storage/Models/CameraInfo.cs
@@ -35,6 +35,6 @@ namespace HucaresServer.Storage.Models
         /// Determines whether this source should still be used for parsing images.
         /// When cameras are unneeded they cannot be deleted until all records referencing the camera are deleted.
         /// </summary>
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }

6080fcc1f47646825d5a3a630fcedfa7bbb28952
Donatas <donatas.tamosauskas@gmail.com>
2018-09-25 16:22:37 +0300

    Implemented single method for testing things out

--- src/HucaresServer.Storage/Helpers/DetectedPlateHelper.cs
+++ src/HucaresServer.Storage/Helpers/DetectedPlateHelper.cs
@@ -9,10 +9,29 @@ namespace HucaresServer.Storage.Helpers
 {
     public class DetectedPlateHelper : IDetectedPlateHelper
     {
+        
+        private IDbContextFactory _dbContextFactory;
 
+        public DetectedPlateHelper(IDbContextFactory dbContextFactory = null)
+        {
+            _dbContextFactory = dbContextFactory ?? new DbContextFactory();
+        }
+        
+        /// <summary>
+        /// Inserts DetectedLicensePlate object into the database.
+        /// </summary>
+        /// <param name="licensePlateToInsert"> DetectedLicensePlate object to insert. </param>
+        /// <returns> Inserted object. </returns>
         public DetectedLicensePlate InsertNewDetectedPlate(DetectedLicensePlate licensePlateToInsert)
         {
-            throw new NotImplementedException();
+            
+            using (var ctx = _dbContextFactory.BuildHucaresContext())
+            {
+                ctx.DetectedLicensePlates.Add(licensePlateToInsert);
+                ctx.SaveChanges();
+            }
+
+            return licensePlateToInsert;    
         }
 
 

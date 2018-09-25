758bdcefec1d4796bfe68ee84b467d219f621bc0
Donatas <donatas.tamosauskas@gmail.com>
2018-09-25 15:50:18 +0300

    Placeholder methods for implementation.

--- src/HucaresServer.Storage/Helpers/DetectedPlateHelper.cs
+++ src/HucaresServer.Storage/Helpers/DetectedPlateHelper.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HucaresServer.Storage.Models;
 
 namespace HucaresServer.Storage.Helpers
 {
-    public class DetectedPlateHelper
+    public class DetectedPlateHelper : IDetectedPlateHelper
     {
+
+        public DetectedLicensePlate InsertNewDetectedPlate(DetectedLicensePlate licensePlateToInsert)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<DetectedLicensePlate> DeletePlatesOlderThanDatetime(DateTime olderThanDatetime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DetectedLicensePlate> GetAllDetectedPlates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DetectedLicensePlate> GetAllDetectedPlatesByPlateNumber(String plateNumber,
+            DateTime? startDateTime = null, DateTime? endDateTime = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DetectedLicensePlate> GetAllDetectedPlatesByCamera(int cameraId,
+            DateTime? startDateTime = null, DateTime? endDateTime = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

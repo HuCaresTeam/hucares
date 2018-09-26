48f1cfbbe18669917832911fab824552a63be290
Donatas <donatas.tamosauskas@gmail.com>
2018-09-26 20:18:02 +0300

    Placeholder unit test method

---
+++ src/HucaresServer.Storage.UnitTests/DetecteedPlateHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HucaresServer.Storage.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HucaresServer.Storage.UnitTests
+{
+    [TestClass]
+    class DetecteedPlateHelperTests
+    {
+
+        [TestMethod]
+        public void InsertNewDetectedPlate_WhenAllDataCorrect_ShouldSucceedAndReturn()
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}

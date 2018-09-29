f0992fc71166999f3a3ca79fed308b021dc4a079
ainoraz <ainoraz@gmail.com>
2018-09-30 02:05:26 +0300

    Added camera info controller and appropriate data models

---
+++ src/HucaresServer/Controllers/CameraInfoController.cs
@@ -0,0 +1,67 @@
+﻿using HucaresServer.Storage.Helpers;
+using System.Web.Http;
+using static HucaresServer.Models.CameraInfoDataModels;
+
+namespace HucaresServer.Controllers
+{
+    public class CameraInfoController : ApiController
+    {
+        public CameraInfoHelper CameraInfoHelper { get; set; } = new CameraInfoHelper();
+
+        [HttpPost]
+        [Route("api/camera/insert")]
+        public IHttpActionResult InsertCamera([FromBody] InsertCameraDataModel data)
+        {
+            return Json(CameraInfoHelper.InsertCamera(data.HostUrl, data.Latitude, data.Longitude, data.IsTrustedSource));
+        }
+
+        [HttpPost]
+        [Route("api/camera/update/source/{id}")]
+        public IHttpActionResult UpdateCameraSource(int id, [FromBody] UpdateCameraSourceDataModel data)
+        {
+            return Json(CameraInfoHelper.UpdateCameraSource(id, data.HostUrl, data.IsTrustedSource));
+        }
+
+        [HttpPost]
+        [Route("api/camera/update/activity/{id}")]
+        public IHttpActionResult UpdateCameraActivity(int id, [FromBody] bool isActive)
+        {
+            return Json(CameraInfoHelper.UpdateCameraActivity(id, isActive));
+        }
+
+        [HttpPost]
+        [Route("api/camera/delete/{id}")]
+        public IHttpActionResult DeleteCameraById(int id)
+        {
+            return Json(CameraInfoHelper.DeleteCameraById(id));
+        }
+
+        [HttpGet]
+        [Route("api/camera/all")]
+        public IHttpActionResult GetAllCameras(bool? isTrustedSource = null)
+        {
+            return Json(CameraInfoHelper.GetAllCameras(isTrustedSource));
+        }
+
+        [HttpGet]
+        [Route("api/camera/active")]
+        public IHttpActionResult GetActiveCameras(bool? isTrustedSource = null)
+        {
+            return Json(CameraInfoHelper.GetActiveCameras(isTrustedSource));
+        }
+
+        [HttpGet]
+        [Route("api/camera/inactive")]
+        public IHttpActionResult GetInactiveCameras()
+        {
+            return Json(CameraInfoHelper.GetInactiveCameras());
+        }
+
+        [HttpGet]
+        [Route("api/camera/{id}")]
+        public IHttpActionResult GetCameraById(int id)
+        {
+            return Json(CameraInfoHelper.GetCameraById(id));
+        }
+    }
+}
---
+++ src/HucaresServer/Models/CameraInfoDataModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HucaresServer.Models
+{
+    public class CameraInfoDataModels
+    {
+        public struct InsertCameraDataModel
+        {
+            public string HostUrl { get; set; }
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+            public bool IsTrustedSource { get; set; }
+        }
+
+        public struct UpdateCameraSourceDataModel
+        {
+            public string HostUrl { get; set; }
+            public bool IsTrustedSource { get; set; }
+        }
+    }
+}

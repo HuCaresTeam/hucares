0b3f0aaab91d7e9d9eae55ea5f613984fc39a0de
Andrius <Andrius@DESKTOP-J39E7VJ>
2018-10-10 19:34:11 +0100

    MLP implementation

---
+++ HucaresWF/src/HucaresWF/MissingLicensePlateForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace HucaresWF
+{
+    partial class MissingLicensePlateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MissingLicensePlateForm));
+            this.licensePlateImage = new System.Windows.Forms.PictureBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.mlpLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.licensePlateImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // licensePlateImage
+            // 
+            this.licensePlateImage.Image = ((System.Drawing.Image)(resources.GetObject("licensePlateImage.Image")));
+            this.licensePlateImage.Location = new System.Drawing.Point(12, 64);
+            this.licensePlateImage.Name = "licensePlateImage";
+            this.licensePlateImage.Size = new System.Drawing.Size(741, 167);
+            this.licensePlateImage.TabIndex = 0;
+            this.licensePlateImage.TabStop = false;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(273, 266);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(340, 59);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "My car has been stolen!";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mlpLabel
+            // 
+            this.mlpLabel.AutoSize = true;
+            this.mlpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mlpLabel.Location = new System.Drawing.Point(329, 9);
+            this.mlpLabel.Name = "mlpLabel";
+            this.mlpLabel.Size = new System.Drawing.Size(251, 31);
+            this.mlpLabel.TabIndex = 2;
+            this.mlpLabel.Text = "Input MLP number";
+            this.mlpLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 93F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(85, 72);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(325, 148);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 93F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(463, 72);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(275, 148);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MissingLicensePlateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 337);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mlpLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.licensePlateImage);
+            this.Name = "MissingLicensePlateForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MissingLicensePlateForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.licensePlateImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox licensePlateImage;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label mlpLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ HucaresWF/src/HucaresWF/MissingLicensePlateForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HucaresWF
+{
+    public partial class MissingLicensePlateForm : Form
+    {
+        public MissingLicensePlateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MissingLicensePlateForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

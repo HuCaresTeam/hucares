d7db2da40cd2c82339b899b92070d3cab2fefe9f
Donatas <donatas.tamosauskas@gmail.com>
2018-10-16 13:10:22 +0300

    Created interface IDetectedPlateClient

---
+++ HucaresWF/src/Hucares.Server.Client/IDetectedPlateClient.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Hucares.Server.Client.Models;
+
+namespace Hucares.Server.Client
+{
+    public interface IDetectedPlateClient
+    {
+        Task<IEnumerable<DetectedLicensePlate>> GetAllDetectedMissingPlates();
+
+        Task<IEnumerable<DetectedLicensePlate>> GetAllDetectedPlatesByPlateNumber(string plateNumber,
+            DateTime? startDateTime = null, DateTime? endDateTime = null);
+
+        Task<IEnumerable<DetectedLicensePlate>> GetAllDetectedPlatesByCamera(int cameraId, DateTime? startDateTime = null, 
+            DateTime? endDateTime = null);
+
+
+    }
+}

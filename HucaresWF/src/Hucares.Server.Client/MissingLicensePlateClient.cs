987ec377566226ae42312ce2371c6a2b0231b88c
Andrius <andrejus.voitovas@gmail.com>
2018-10-16 21:41:37 +0300

    MLP client implementation

---
+++ HucaresWF/src/Hucares.Server.Client/MissingLicensePlateClient.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Hucares.Server.Client.Models;
+using Newtonsoft.Json;
+
+namespace Hucares.Server.Client
+{
+    public class MissingLicensePlateClient : IMissingLicensePlateClient
+    {
+        private Uri HostUri { get; set; } = new Uri("http://localhost:50510");
+        
+        private readonly HttpClientHelper httpHelper;
+        
+        public MissingLicensePlateClient(HttpClientHelper httpHelper = null)
+        {
+            this.httpHelper = httpHelper ?? new HttpClientHelper();
+        }
+        
+        public async Task<MissingLicensePlate> InsertPlateRecord(string plateNumber, DateTime searchStartDatetime)
+        {
+            var uri = $"api/mlp/insert/{plateNumber}";
+            var fullUri = new Uri(HostUri, uri);
+
+            var plateNumberData = new
+            {
+                plateNumber = plateNumber,
+                searchStartDatetime = searchStartDatetime
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(plateNumberData, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+
+            return await httpHelper.MakeRequest<MissingLicensePlate>(request);
+        }
+
+        public async Task<IEnumerable<MissingLicensePlate>> GetAllPlateRecords()
+        {
+            var uri = $"api/mlp/all";
+            var fullUri = new Uri(HostUri, uri);
+            var request = new HttpRequestMessage(HttpMethod.Get, fullUri);
+            return await httpHelper.MakeRequest<IEnumerable<MissingLicensePlate>>(request);
+        }
+
+        public async Task<IEnumerable<MissingLicensePlate>> GetPlateRecordByPlateNumber(string plateNumber)
+        {
+            var uri = $"api/mlp/plate/{plateNumber}";
+            var fullUri = new Uri(HostUri, uri);
+            
+            var jsonContent = JsonConvert.SerializeObject(plateNumber, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+            
+            return await httpHelper.MakeRequest<IEnumerable<MissingLicensePlate>>(request);
+        }
+
+        public async Task<MissingLicensePlate> UpdatePlateRecord(int plateId, string plateNumber, DateTime searchStartDatetime)
+        {
+            var uri = $"api/mlp/update/{plateId}";
+            var fullUri = new Uri(HostUri, uri);
+
+            var plateNumberData = new
+            {
+                plateId = plateId,
+                plateNumber = plateNumber,
+                searchStartDatetime = searchStartDatetime
+            };
+            
+            var jsonContent = JsonConvert.SerializeObject(plateNumberData, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+
+            return await httpHelper.MakeRequest<MissingLicensePlate>(request);
+        }
+
+        public async Task<MissingLicensePlate> MarkFoundPlate(int plateId, DateTime requestDateTime, bool isFound)
+        {
+            var uri = $"api/mlp/found/{plateId}/{isFound}";
+            var fullUri = new Uri(HostUri, uri);
+            
+            var plateNumberData = new
+            {
+                plateId = plateId,
+                requestDateTime = requestDateTime,
+                isFound = isFound
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(plateNumberData, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+
+            return await httpHelper.MakeRequest<MissingLicensePlate>(request);
+        }
+
+        public async Task<MissingLicensePlate> DeletePlateById(int plateId)
+        {
+            var uri = $"api/mlp/delete/{plateId}";
+            var fullUri = new Uri(HostUri, uri);
+
+            var jsonContent = JsonConvert.SerializeObject(plateId, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+
+            return await httpHelper.MakeRequest<MissingLicensePlate>(request);
+        }
+
+        public async Task<MissingLicensePlate> DeletePlateByNumber(string plateNumber)
+        {
+            var uri = $"api/mlp/delete/{plateNumber}";
+            var fullUri = new Uri(HostUri, uri);
+
+            var jsonContent = JsonConvert.SerializeObject(plateNumber, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
+            var request = new HttpRequestMessage(HttpMethod.Post, fullUri)
+            {
+                Content = new StringContent(jsonContent, Encoding.UTF8, "application/json")
+            };
+
+            return await httpHelper.MakeRequest<MissingLicensePlate>(request);
+        }
+    }
+}

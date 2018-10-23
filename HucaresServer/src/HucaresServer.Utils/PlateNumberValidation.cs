f2a911b87a086527f7447b857b21c87b88ec17c3
Andrius <andrejus.voitovas@gmail.com>
2018-10-23 23:30:31 +0300

    Changed regex and on update post to put

--- HucaresServer/src/HucaresServer.Utils/PlateNumberValidation.cs
+++ HucaresServer/src/HucaresServer.Utils/PlateNumberValidation.cs
@@ -12,7 +12,7 @@ namespace HucaresServer.Utils
     {
         public static bool IsValidPlateNumber(this string str)
         {
-            var rx = new Regex(@"^\w{3}\d{3}$");
+            var rx = new Regex(@"^([A-Z]){3}\d{3}$");
             return rx.IsMatch(str);
         }
     }

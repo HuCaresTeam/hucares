8f26f25ff5bf06bd8e704173dd461be6d65d6632
ainoraz <ainoraz@gmail.com>
2018-10-24 23:43:16 +0300

    Refactored UI code, added events

---
+++ HucaresWF/src/HucaresWF/CameraTab.cs
@@ -0,0 +1,36 @@
+﻿using Hucares.Server.Client.Models;
+using HucaresWF.Properties;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HucaresWF
+{
+    public partial class HucaresForm : Form
+    {
+        private async Task UpdateCameraDataSource()
+        {
+            cameraTable.DataSource = await cameraClient.GetAllCameras();
+        }
+
+        private async void submitCam_Click(object sender, EventArgs e)
+        {
+            if (!Uri.IsWellFormedUriString(hostUrlField.Text, UriKind.Absolute))
+            {
+                var eventArgs = new ExceptionEventArgs { ExceptionMessage = Resources.Error_BadUri };
+                ExceptionEventHandler?.Invoke(this, eventArgs);
+                return;
+            };
+            var camObj = new CameraInfo()
+            {
+                HostUrl = hostUrlField.Text,
+                Latitude = (double)latField.Value,
+                Longitude = (double)longField.Value,
+                IsTrustedSource = isTrustedBox.Checked
+            };
+
+            await cameraClient.InsertCamera(camObj);
+            await UpdateCameraDataSource();
+        }
+    }
+}
---
+++ HucaresWF/src/HucaresWF/MlpTab.cs
@@ -0,0 +1,30 @@
+﻿using HucaresWF.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace HucaresWF
+{
+    public partial class HucaresForm : Form
+    {
+        private void showMlpList_Click(object sender, EventArgs e)
+        {
+            MissingLicensePlateList mlpList = MissingLicensePlateList.CreateIfNotExistsAndGetInstance(mlpClient, this);
+            mlpList.Show();
+        }
+
+        private async void submitButton_Click(object sender, EventArgs e)
+        {
+            var plateNumber = lettersBox.Text + digitsBox.Text;
+            if (plateNumber.ValidatePlateNumber())
+            {
+                var eventArgs = new ExceptionEventArgs { ExceptionMessage = Resources.Error_BadUri };
+                ExceptionEventHandler?.Invoke(this, eventArgs);
+                return;
+            }
+
+            await mlpClient.InsertPlateRecord(plateNumber, DateTime.Now);
+            await MissingLicensePlateList.CreateIfNotExistsAndGetInstance(mlpClient, this).UpdateDatasource();
+            await UpdateDlpDataSource();
+        }
+    }
+}

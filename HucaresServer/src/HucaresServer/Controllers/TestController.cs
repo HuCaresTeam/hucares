7ffcc340c8da2732ef786a57920998bd1d39c7a9
Donatas <donatas.tamosauskas@gmail.com>
2018-10-24 21:22:06 +0300

    Added test controllers to generate small amount of dummy data.

--- HucaresServer/src/HucaresServer/Controllers/TestController.cs
+++ HucaresServer/src/HucaresServer/Controllers/TestController.cs
@@ -1,7 +1,9 @@
 ﻿using HucaresServer.Storage;
 using HucaresServer.Storage.Models;
 using System;
+using System.Collections.Generic;
 using System.Web.Http;
+using HucaresServer.Storage.Helpers;
 
 namespace HucaresServer.Controllers
 {
@@ -14,6 +16,55 @@ namespace HucaresServer.Controllers
         public IHttpActionResult Get(int id)
         {
             return Json($"Hello world {id}");
+        }
+        
+        
+        [HttpGet]
+        [Route("api/test/generate/mlp")]
+        public IHttpActionResult GenerateFakeMLPData()
+        {
+            var missingPlateHelper = new MissingPlateHelper();
+            
+            missingPlateHelper.InsertPlateRecord("JEZ123", new DateTime(2018, 10, 16));
+            missingPlateHelper.InsertPlateRecord("ABG696", new DateTime(2018, 10, 20));
+            missingPlateHelper.InsertPlateRecord("BDI911", new DateTime(2018, 10, 24));
+            missingPlateHelper.InsertPlateRecord("HAH666", new DateTime(2018, 10, 25));
+            
+            return Json("Fake MLP data generated");
+        }
+        
+        [HttpGet]
+        [Route("api/test/generate/dlp")]
+        public IHttpActionResult GenerateFakeDLPData()
+        {
+            var detectedPlateHelper = new DetectedPlateHelper();
+
+            detectedPlateHelper.InsertNewDetectedPlate("JEZ123", new DateTime(2018, 10, 22), 1, "ipfs://hucares.io/detections/camera/28/20181022", 0.86);
+            detectedPlateHelper.InsertNewDetectedPlate("JEZ123", new DateTime(2018, 10, 22), 2, "ipfs://hucares.io/detections/camera/23/20181022", 0.92);
+            detectedPlateHelper.InsertNewDetectedPlate("JEZ123", new DateTime(2018, 10, 22), 1, "ipfs://hucares.io/detections/camera/28/20181022", 0.73);
+            
+            detectedPlateHelper.InsertNewDetectedPlate("BDI911", new DateTime(2018, 10, 20), 1, "ipfs://hucares.io/detections/camera/28/20181022", 0.71);
+            detectedPlateHelper.InsertNewDetectedPlate("BDI911", new DateTime(2018, 10, 21), 1, "ipfs://hucares.io/detections/camera/28/20181022", 0.98);
+            detectedPlateHelper.InsertNewDetectedPlate("BDI911", new DateTime(2018, 10, 22), 1, "ipfs://hucares.io/detections/camera/28/20181022", 0.95);
+            detectedPlateHelper.InsertNewDetectedPlate("BDI911", new DateTime(2018, 10, 23), 2, "ipfs://hucares.io/detections/camera/23/20181022", 0.78);
+            
+            detectedPlateHelper.InsertNewDetectedPlate("HAH666", new DateTime(2018, 10, 25), 2, "ipfs://hucares.io/detections/camera/23/20181022", 0.999);
+            
+            return Json("Fake DLP data generated");
+        }
+        
+        [HttpGet]
+        [Route("api/test/generate/camera")]
+        public IHttpActionResult GenerateFakeCameraData()
+        {
+            var cameraInfoHelper = new CameraInfoHelper();
+
+            cameraInfoHelper.InsertCamera("http://map.sviesoforai.lt/camera/api/camera/Camera_028.jpg", 54.736062,
+                25.264113, true);
+            cameraInfoHelper.InsertCamera("http://map.sviesoforai.lt/camera/api/camera/Camera_023.jpg", 54.701329, 
+                25.262231, true);
+ 
+            return Json("Fake Camera data generated");
         }
 
         /// <summary>

508187bd1e5667349341921a5ff82782635d20ce
Donatas <donatas.tamosauskas@gmail.com>
2018-11-04 16:28:56 +0200

    Created CameraImageDownloading interface and class that will implement that interface.

--- HucaresServer/src/HucaresServer.DataAcquisition/ICameraImageDownloading.cs
+++ HucaresServer/src/HucaresServer.DataAcquisition/ICameraImageDownloading.cs
@@ -1,7 +1,13 @@
 namespace HucaresServer.DataAcquisition
 {
     public interface ICameraImageDownloading
-    {
+    {    
+        /// <summary>
+        /// Downloads image from every source available in CameraInfo table. Optionally can be filtered by isTrusted value.
+        /// </summary>
+        /// <param name="isTrusted"> Optional parameter indicating if the source of image if verified.</param>
+        /// <returns> Number of images downloaded.</returns>
+        int DownloadImagesFromCameraInfoSources(bool? isTrusted = null);
         
     }
 }

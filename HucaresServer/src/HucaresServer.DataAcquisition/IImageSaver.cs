1ab799762e2dde3fc917c9858be50c9b012498dd
Donatas <donatas.tamosauskas@gmail.com>
2018-11-04 20:59:25 +0200

    Changed GenerateFileName to a private method

--- HucaresServer/src/HucaresServer.DataAcquisition/IImageSaver.cs
+++ HucaresServer/src/HucaresServer.DataAcquisition/IImageSaver.cs
@@ -10,14 +10,5 @@ namespace HucaresServer.DataAcquisition
         /// </summary>
         /// <param name="imageToSave"> Bitmap of an image to save</param>
         void SaveImage(Bitmap imageToSave);
-
-        /// <summary>
-        /// Method generates a filename to be used for an image
-        /// </summary>
-        /// <param name="cameraId"> Id of the camera that took the picture.</param>
-        /// <param name="captureDateTime"> Optional parameter for the datetime of when the picture was taken. If not
-        /// specified the current datetime will be used.</param>
-        /// <returns> The generated filename for the image.</returns>
-        string GenerateFileName(int cameraId, DateTime? captureDateTime = null);
     }
 }
--- HucaresServer/src/HucaresServer.DataAcquisition/LocalImageSaver.cs
+++ HucaresServer/src/HucaresServer.DataAcquisition/LocalImageSaver.cs
@@ -17,7 +17,14 @@ namespace HucaresServer.DataAcquisition
             throw new NotImplementedException();
         }
 
-        public string GenerateFileName(int cameraId, DateTime? captureDateTime = null)
+        /// <summary>
+        /// Method generates a filename to be used for an image
+        /// </summary>
+        /// <param name="cameraId"> Id of the camera that took the picture.</param>
+        /// <param name="captureDateTime"> Optional parameter for the datetime of when the picture was taken. If not
+        /// specified the current datetime will be used.</param>
+        /// <returns> The generated filename for the image.</returns>
+        private string GenerateFileName(int cameraId, DateTime? captureDateTime = null)
         {
             throw new NotImplementedException();
         }

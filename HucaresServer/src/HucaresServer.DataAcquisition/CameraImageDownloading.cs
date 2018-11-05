1447005d9da22a74d0bcacb4459ac85ba3baa163
Donatas <donatas.tamosauskas@gmail.com>
2018-11-05 21:19:00 +0200

    Implemented DownloadImagesFromCameraInfoSources method.

--- HucaresServer/src/HucaresServer.DataAcquisition/CameraImageDownloading.cs
+++ HucaresServer/src/HucaresServer.DataAcquisition/CameraImageDownloading.cs
@@ -1,20 +1,38 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using HucaresServer.Storage.Helpers;
 
 namespace HucaresServer.DataAcquisition
 {
     public class CameraImageDownloading : ICameraImageDownloading
     {
-        private  ICameraInfoHelper _cameraInfoHelper;
+        // TODO: Take this value from a configuration file or somewhere else
+        private const string TemporaryStorageUrl = "/temporaryImages";
 
-        public CameraImageDownloading(ICameraInfoHelper cameraInfoHelper = null)
+        private ICameraInfoHelper _cameraInfoHelper;
+        private IImageSaver _imageSaver;
+
+        public CameraImageDownloading(ICameraInfoHelper cameraInfoHelper = null, IImageSaver imageSaver = null)
         {
             _cameraInfoHelper = cameraInfoHelper ?? new CameraInfoHelper();
+            _imageSaver = imageSaver ?? new LocalImageSaver();
         }
-        
+
         public int DownloadImagesFromCameraInfoSources(bool? isTrusted = null, DateTime? downloadDateTime = null)
         {
-            throw new System.NotImplementedException();
+            var cameraDataToDownload = _cameraInfoHelper.GetActiveCameras(isTrusted = isTrusted).ToList();
+
+            using (var webClient = new WebClient())
+            {
+                foreach (var cameraData in cameraDataToDownload)
+                {
+                    webClient.DownloadFileAsync(new Uri(cameraData.HostUrl),
+                        _imageSaver.GenerateStorageLocationAndFilename(cameraData.Id, downloadDateTime));
+                }
+            }
+
+            return cameraDataToDownload.Count;
         }
     }
 }

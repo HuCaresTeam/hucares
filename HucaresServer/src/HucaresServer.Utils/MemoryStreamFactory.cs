1fc75aecab78273fc267d788773c2f626a6a0ce7
ainoraz <ainoraz@gmail.com>
2018-11-07 00:13:17 +0200

    Added memory stream factory

---
+++ HucaresServer/src/HucaresServer.Utils/MemoryStreamFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HucaresServer.Utils
+{
+    public class MemoryStreamFactory : IMemoryStreamFactory
+    {
+        public MemoryStream Create()
+        {
+            return new MemoryStream();
+        }
+    }
+}

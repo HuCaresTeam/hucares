0aef76ee3c3dcb885b8b03f843a8b96d5b7fa8bd
Donatas <donatas.tamosauskas@gmail.com>
2018-11-07 19:46:29 +0200

    Fixed test to run on a windows machine.

--- HucaresServer/src/HucaresServer.DataAcquisition.UnitTests/CameraInfoDownloadingTests.cs
+++ HucaresServer/src/HucaresServer.DataAcquisition.UnitTests/CameraInfoDownloadingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using FakeItEasy;
@@ -16,14 +17,12 @@ namespace HucaresServer.DataAcquisition.UnitTests
         public void DownloadImagesFromCameraInfoSources_WithTrusted_ShouldDownloadAndReturn()
         {
             // Arrange
-            var fakeImageArray = new byte[100];
-            Bitmap fakeBitmap;
+            var fakeBitmap = new Bitmap(100, 100);
             var url = "https://some.url";
             
-            using (var stream = new MemoryStream(fakeImageArray))
-            {
-                fakeBitmap = new Bitmap(stream);
-            }
+            ImageConverter converter = new ImageConverter();
+            var fakeImageArray =  (byte[])converter.ConvertTo(fakeBitmap, typeof(byte[]));
+            
             
             var fakeWebClient = A.Fake<IWebClient>();
             A.CallTo(() => fakeWebClient.DownloadData(url))
@@ -35,7 +34,7 @@ namespace HucaresServer.DataAcquisition.UnitTests
             
             var fakeMissingPlates = new List<CameraInfo>()
             {
-                new CameraInfo() {HostUrl = url, IsTrustedSource = true},
+                new CameraInfo() {Id = 0, HostUrl = url, IsTrustedSource = true},
             };
             
             var fakeCameraInfoHelper = A.Fake<ICameraInfoHelper>();
@@ -47,12 +46,12 @@ namespace HucaresServer.DataAcquisition.UnitTests
             var cameraImageDownloading = new CameraImageDownloading(fakeCameraInfoHelper, fakeImageSaver, fakeWebClientFactory);
             
             // Act
-            var resultCameraCount = cameraImageDownloading.DownloadImagesFromCameraInfoSources(isTrusted: true);
+            var resultCameraCount = cameraImageDownloading.DownloadImagesFromCameraInfoSources(true, new DateTime(2018, 11, 01));
             
             // Assert
             A.CallTo(() => fakeCameraInfoHelper.GetActiveCameras(true)).MustHaveHappened();
             A.CallTo(() => fakeWebClient.DownloadData(url)).MustHaveHappenedOnceExactly();
-//            A.CallTo(() => fakeImageSaver.SaveImage(fakeBitmap)).MustHaveHappened();
+//            A.CallTo(() => fakeImageSaver.SaveImage(fakeBitmap, 0, new DateTime(2018, 11, 01))).MustHaveHappenedOnceExactly();
             resultCameraCount.ShouldBe(1);
         }
     }

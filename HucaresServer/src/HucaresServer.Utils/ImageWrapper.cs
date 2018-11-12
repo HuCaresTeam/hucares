48a2888bd9cc292ba467c4e33c2042cbd5094e6b
ainoraz <ainoraz@gmail.com>
2018-11-13 00:33:44 +0200

    Updated OpenAlprWrapper to use ImageWrapper

---
+++ HucaresServer/src/HucaresServer.Utils/ImageWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HucaresServer.Utils
+{
+    public class ImageWrapper : IImageWrapper
+    {
+        public Image GetImageFromFile(string pathToPlateImage)
+        {
+            return Image.FromFile(pathToPlateImage);
+        }
+    }
+}

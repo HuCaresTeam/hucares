4d480d2cf6f06c61674d66e94a8e0855c28ec044
ainoraz <ainoraz@gmail.com>
2018-11-13 00:34:29 +0200

    Added hangfire to project and made it call DlpCollectionProcess every minute.

---
+++ HucaresServer/src/HucaresServer.TimedProccess/DlpCollectionProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HucaresServer.TimedProcess
+{
+    public class DlpCollectionProcess
+    {
+        public static async Task StartProccess()
+        {
+            await Task.Delay(1);
+            throw new NotImplementedException();
+        }
+    }
+}

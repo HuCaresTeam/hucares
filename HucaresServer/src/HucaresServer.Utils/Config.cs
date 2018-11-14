017c6473c97610dbcb8553aa95264a7c0636f5b8
ainoraz <ainoraz@gmail.com>
2018-11-14 21:55:43 +0200

    Added config

---
+++ HucaresServer/src/HucaresServer.Utils/Config.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+
+namespace HucaresServer.Utils
+{
+    public static class Config
+    {
+        public static string AlprSecretKey {
+            get { return ConfigurationManager.AppSettings["AlprSecretKey"]; }
+        }
+
+        public static string AlprCountry {
+            get {
+                return ConfigurationManager.AppSettings["AlprCountry"];
+            }
+        }
+        public static int AlprRecognizeVehicle {
+            get {
+                return int.Parse(ConfigurationManager.AppSettings["AlprRecognizeVehicle"]);
+            }
+        }
+        public static string AlprState {
+            get {
+                return ConfigurationManager.AppSettings["AlprState"];
+            }
+        }
+        public static int AlprReturnImage {
+            get {
+                return int.Parse(ConfigurationManager.AppSettings["AlprReturnImage"]);
+            }
+        }
+        public static int AlprTopn {
+            get {
+                return int.Parse(ConfigurationManager.AppSettings["AlprTopn"]);
+            }
+        }
+        public static string AlprPrewarp {
+            get {
+                return ConfigurationManager.AppSettings["AlprPrewarp"];
+            }
+        }
+    }
+}

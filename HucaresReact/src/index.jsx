f185b9058858afe6d08ee34ee4efa956864fc7af
Donatas <donatas.tamosauskas@gmail.com>
2018-11-29 10:45:37 +0200

    Added clipper drawer

--- HucaresReact/src/index.jsx
+++ HucaresReact/src/index.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './root/App';
+import ClippedDrawer from './components/Sidebar/MaterialSidebar';
 
-render(<App />, document.getElementById('app'));
+render(<ClippedDrawer />, document.getElementById('app'));

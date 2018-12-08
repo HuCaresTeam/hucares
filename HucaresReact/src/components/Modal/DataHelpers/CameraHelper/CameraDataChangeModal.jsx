4298f7c594d903226af71d92b209a5ba3691cfa6
Andrejus Voitovas <andrejus.voitovas@nfq.lt>
2018-12-08 12:43:54 +0200

    Camera, MLP setters implementation

--- HucaresReact/src/components/Modal/DataHelpers/CameraHelper/CameraDataChangeModal.jsx
+++ HucaresReact/src/components/Modal/DataHelpers/CameraHelper/CameraDataChangeModal.jsx
@@ -4,6 +4,16 @@ import 'semantic-ui-css/semantic.min.css';
 import styles from '../../Modal.scss';
 
 export class CameraDataChangeModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      HostUrl: props.data.HostUrl,
+      Latitude: props.data.Latitude,
+      Longitude: props.data.Longitude,
+      IsTrustedSource: props.data.IsTrustedSource,
+    };
+  }
+
   render() {
     return (
       <Modal trigger={<Button primary>Update</Button>} className={styles.modalPosition}>
@@ -11,20 +21,33 @@ export class CameraDataChangeModal extends React.Component {
           <Form>
             <Form.Field>
               <label>Camera Host URL</label>
-              <input placeholder="Host URL" value={this.props.data.HostUrl} />
+              <input
+                placeholder="Host URL"
+                value={this.state.HostUrl}
+                onChange={e => this.setState({ HostUrl: e.target.value })}
+              />
             </Form.Field>
             <Form.Field>
               <label>Latitude</label>
-              <input placeholder="Latitude" value={this.props.data.Latitude} />
+              <input
+                placeholder="Latitude"
+                value={this.state.Latitude}
+                onChange={e => this.setState({ Latitude: e.target.value })}
+              />
             </Form.Field>
             <Form.Field>
               <label>Longitude</label>
-              <input placeholder="Longitude" value={this.props.data.Longitude} />
+              <input
+                placeholder="Longitude"
+                value={this.state.Longitude}
+                onChange={e => this.setState({ Longitude: e.target.value })}
+              />
             </Form.Field>
             <Form.Field>
               <Checkbox
                 label="This camera is trusted source"
-                checked={this.props.data.IsTrustedSource}
+                checked={this.state.IsTrustedSource}
+                onChange={e => this.setState({ IsTrustedSource: e.target.value })}
               />
             </Form.Field>
             <Button type="submit">Submit</Button>
--- HucaresReact/src/components/Modal/DataHelpers/MLPHelpers/MLPDataChangeModal.jsx
+++ HucaresReact/src/components/Modal/DataHelpers/MLPHelpers/MLPDataChangeModal.jsx
@@ -4,6 +4,16 @@ import 'semantic-ui-css/semantic.min.css';
 import styles from '../../Modal.scss';
 
 export class MLPDataChangeModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      PlateNumber: props.data.PlateNumber,
+      SearchStartDateTime: props.data.SearchStartDateTime,
+      SearchEndDateTime: props.data.SearchEndDateTime,
+      Status: props.data.Status,
+    };
+  }
+
   render() {
     return (
       <Modal trigger={<Button primary>Update</Button>} className={styles.modalPosition}>
@@ -11,18 +21,26 @@ export class MLPDataChangeModal extends React.Component {
           <Form>
             <Form.Field>
               <label>Missing License Plate</label>
-              <input placeholder="License plate" value={this.props.data.PlateNumber} />
+              <input
+                placeholder="License plate"
+                value={this.state.PlateNumber}
+                onChange={e => this.setState({ PlateNumber: e.target.value })}
+              />
             </Form.Field>
             <Form.Field>
               <label>Search Start Date</label>
-              <input readOnly value={this.props.data.SearchStartDateTime} />
+              <input readOnly value={this.state.SearchStartDateTime} />
             </Form.Field>
             <Form.Field>
               <label>Search End Date</label>
-              <input readOnly value={this.props.data.SearchEndDateTime} />
+              <input readOnly value={this.state.SearchEndDateTime} />
             </Form.Field>
             <Form.Field>
-              <Checkbox label="This license plate was found" checked={this.props.data.Status} />
+              <Checkbox
+                label="This license plate was found"
+                checked={this.state.Status}
+                onChange={e => this.setState({ Status: e.target.value })}
+              />
             </Form.Field>
             <Button type="submit">Submit</Button>
           </Form>

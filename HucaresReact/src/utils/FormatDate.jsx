f67ddcd518a5515020493bec372acd8b139ea05e
Andrejus Voitovas <andrejus.voitovas@nfq.lt>
2018-12-16 23:09:33 +0200

    Dates fix

--- HucaresReact/src/utils/FormatDate.jsx
+++ HucaresReact/src/utils/FormatDate.jsx
@@ -1,5 +1,12 @@
 export function formatDate(date) {
-  const DATE_OPTIONS = { year: 'numeric', month: 'long', day: 'numeric' };
+  const DATE_OPTIONS = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
   const dateToFormat = new Date(date);
-  return dateToFormat.toLocaleDateString('en-US', DATE_OPTIONS);
+  return dateToFormat.toLocaleDateString('en-GB', DATE_OPTIONS);
 }
